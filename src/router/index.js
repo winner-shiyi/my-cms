f5335b6c78b05a0eb9f41a435cb69edb66816441
weina <313614833@qq.com>
2018-03-12 18:32:13 +0800

    增加stylus-loader

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,12 @@ import HelloWorld from '@/components/HelloWorld'
 Vue.use(Router)
 
 export default new Router({
+  // linkActiveClass: 'active', // 当前路由的active
   routes: [
+    // {
+    //   path: '/',
+    //   redirect: '/goods'   // 当访问根目录的时候默认跳转到主页
+    // },
     {
       path: '/',
       name: 'HelloWorld',
